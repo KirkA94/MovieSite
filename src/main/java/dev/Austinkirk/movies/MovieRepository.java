f892b94176dc21b4b794a425bf28a77c7150bcc7
Your Name <you@example.com>
2026-01-31 00:04:43 -0500

    Spring Boot Movies API / created the endpoint for being able to view a single movie by ID, also created the endpoint for movie reviews that allows the users to leave a review on a single movie , post it and then database updates the movie with that review

--- src/main/java/dev/Austinkirk/movies/MovieRepository.java
+++ src/main/java/dev/Austinkirk/movies/MovieRepository.java
@@ -1,9 +1,12 @@
 package dev.Austinkirk.movies;
 
-import org.bson.types.ObjectId;
+import  org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface MovieRepository extends MongoRepository<Movie, ObjectId> {
+    Optional<Movie> findMovieByImdbId(String imdbId);
 }
