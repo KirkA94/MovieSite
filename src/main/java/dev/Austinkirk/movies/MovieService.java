db00e581f0cdebb43937a809e86be3bc11ba7ea6
Your Name <you@example.com>
2026-01-31 00:14:49 -0500

    Spring Boot Movies API / created the endpoint for being able to view a single movie by ID, also created the endpoint for movie reviews that allows the users to leave a review on a single movie , post it and then database updates the movie with that review

--- src/main/java/dev/Austinkirk/movies/MovieService.java
+++ src/main/java/dev/Austinkirk/movies/MovieService.java
@@ -1,7 +1,5 @@
 package dev.Austinkirk.movies;
 
-import jakarta.annotation.PostConstruct;
-import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
